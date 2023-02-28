a6459a0779cca27b268309c05f7c338cc0f1afc5
Sergiu Moga <sergiu.moga@protonmail.com>
2023-02-28 19:59:13 +0200

    plat: Introduce `struct uk_reloc`
    In order to start supporting positional independence we need to be able
    to self relocate. Thus, define a new custom relocation related structure
    `struct uk_reloc` that is meant to hold the offset in memory where the
    relocation is to be applied, the offset of the value from the original
    symbol value computer by the linker through the linker script's base
    reference address, the size of the relocation and a flags field
    respectively (for now we only have the `UK_RELOC_FLAGS_PHYS_REL` flag
    meant to indicate whether the relocation is to be applied based on a
    physical address, which comes in handy in bootstrap code).
    
    The final binary blob that the `mkukreloc.py` script builds is made of
    a signature (UK_RELOC_SIGNATURE), `struct uk_reloc` entries appended
    to each other and ends with a zeroed out `struct uk_reloc` to act as a
    sentinel. This binary blob will be placed in the binary through the
    `.uk_reloc` ELF section, updated through `objcopy`. Therefore, in order
    for this forced section update to not mess up linker script exported
    symbols, this section must be placed towards the end of the binary.
    
    The binary blob is obtained by parsing all of the `.rela.dyn` entries,
    converting them into `stuct uk_reloc` entries and by searching
    through the debug image's symbols for `_uk_reloc_` symbols created with
    the help of the architecture independent helper assembly macro:
    `ur_data`. This macro has been added to aid in replacing the references
    to absolute symbol values with position independent equivalents that,
    instead of referencing this very symbol, it places a placeholder value
    (UK_RELOC_PLACEHOLDER) and generates a symbol to be parsed by the script
    to build a uk_reloc entry. Consider the following usage example:
    `ur_data quad, symbol, 8, _phys`
    This will result in the `nm` tool, whose output the script will parse,
    generating such entry:
    `0000000000100106 T symbol_uk_reloc_data8_phys`
    For `mkukreloc.py` this means, with a linker script reference address
    of 0x100000:
    ```
    struct uk_reloc {
    	__u64 m_off = 0x106;  // 0x100106 - 0x100000
    	__u64 r_val_off;
    	__u32 r_sz = 8;  // symbol_uk_reloc_data[8]_phys
    	__u32 flags = UK_RELOC_FLAGS_PHYS_REL;  // symbol_uk_reloc_data8[_phys]
    } __packed;
    ```
    In order for actual value of `symbol` to be found, since its name is at
    the beginning of the generated symbol, the script will now look for such
    entry:
    `0000000000100078 T symbol`  // from [symbol]_uk_reloc_data8_phys
    and thus, `0x78` becomes the value of `r_val_off` and the
    `struct uk_reloc` is completed and appended to the binary blob with the
    others.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

---
+++ include/uk/uk_reloc.h
@@ -0,0 +1,66 @@
+#ifndef __UK_RELOC_H__
+#define __UK_RELOC_H__
+
+#define UK_RELOC_FLAGS_PHYS_REL	(1 << 0)
+#define UK_RELOC_PLACEHOLDER		0xB00B
+#define UK_RELOC_SIGNATURE		0xBADB0111
+#define UK_RELOC_ALIGNMENT		0x1000
+
+#ifdef __ASSEMBLY__
+
+.macro ur_data type:req, sym:req, bytes:req, phys
+#ifdef CONFIG_OPTIMIZE_PIE
+.globl \sym\()_uk_reloc_data\bytes\()\phys\()
+.set \sym\()_uk_reloc_data\bytes\()\phys\(), .
+	.\type	UK_RELOC_PLACEHOLDER
+#else
+	.\type	\sym
+#endif
+.endm
+
+#ifndef CONFIG_OPTIMIZE_PIE
+do_uk_reloc:
+	ret
+#endif
+
+#else  /* __ASSEMBLY__ */
+
+#include <uk/arch/types.h>
+#include <uk/plat/common/sections.h>
+
+#define __uk_reloc __section(".uk_reloc") __used
+
+struct uk_reloc {
+	__u64 m_off;
+	__u64 r_val_off;
+	__u32 r_sz;
+	__u32 flags;
+} __packed;
+
+#define UK_RELOC_ENTRY(ur_m_off, ur_r_val_off, ur_r_sz, ur_flags)		\
+	{									\
+		.m_off		= (ur_m_off),					\
+		.r_val_off	= (ur_r_val_off),				\
+		.r_sz		= (ur_r_sz),					\
+		.flags		= (ur_flags),					\
+	}
+
+static inline void apply_uk_reloc(volatile struct uk_reloc *ur, __u64 val,
+				  volatile void * volatile baddr)
+{
+	switch (ur->r_sz) {
+	case 2:
+		*(volatile __u16 * volatile) (baddr  + ur->m_off) = (__u16) val;
+		break;
+	case 4:
+		*(volatile __u32 * volatile) (baddr  + ur->m_off) = (__u32) val;
+		break;
+	case 8:
+		*(volatile __u64 * volatile) (baddr  + ur->m_off) = (__u64) val;
+		break;
+	}
+}
+
+#endif /* !__ASSEMBLY__ */
+
+#endif /* __UK_RELOC_H__ */
