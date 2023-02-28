f87878a9cd5b900b4d91a751ccfa99e9d11fb9bf
Sergiu Moga <sergiu.moga@protonmail.com>
2023-02-28 20:55:46 +0200

    plat/common: Implement a `struct uk_reloc` based self relocator
    Add `do_uk_reloc` a method that parses the `.uk_reloc` function and
    applies the relocations accordingly. As arguments, this method receives
    a base physicall address `r_paddr` for relocations that employ the
    `UK_RELOC_FLAGS_PHYS_REL` flag and abase virtual address `r_vaddr`
    for those that do not want to be relocated agains a physical address.
    
    The relocator is able to compute its own, current load time base address
    through `st_curr_baddr`, an architecture dependent method. Therefore,
    each application must provide a method that places into `baddr` the
    current load time base address (which may be different from the two
    addresses passed as arguments).
    
    In the case of `arm64`, this is easily achieved through the position
    independent, instruction pointer relative, instruction `adrp`.
    
    For `x86`, this is done by calculating the negative offset between
    the link time address of `do_uk_reloc` and `_base_addr` and adding
    this negative offset to `do_uk_reloc`, thus obtaining the very value
    of `_base_addr` at runtime.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

---
+++ plat/common/include/x86/uk_reloc.h
@@ -0,0 +1,26 @@
+#ifndef __X86_UK_RELOC_H__
+#define __X86_UK_RELOC_H__
+
+#include <uk/plat/common/sections.h>
+#include <uk/uk_reloc.h>
+
+static inline void st_curr_baddr(volatile void * volatile * baddr)
+{
+	volatile __off offset = 0;
+
+	/* Get negative offset from `do_uk_reloc`'s symbolic value to the base
+	 * of the loaded unikernel
+	*/
+	__asm__ __volatile__(
+		"movq $(_base_addr - do_uk_reloc), %0"
+		:
+		: "m"(offset)
+	);
+
+	/* Add the negative offset to `do_uk_reloc`, thus obtaining our current
+	 * load address
+	 */
+	*baddr = (volatile void *) ((__s64)do_uk_reloc + (__s64)(offset));
+}
+
+#endif /* __X86_UK_RELOC_H__ */
