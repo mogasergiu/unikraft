1ea1b6ed925349a000a55fdd776192c9db2ef2a3
Sergiu Moga <sergiu.moga@protonmail.com>
2023-03-22 21:10:42 +0200

    plat/common/x86: Add `ur_mov` instruction macro
    Due to the high number of references to absolute symbol values in the
    16-bit and 32-bit bootstrap code, a new macro, `ur_mov`, was introduced.
    The new macro creates a new type of `_uk_reloc_` symbol, with an `imm`
    suffix, to show that it is placed right after a `mov` instruction whose
    immediate value must be relocated/patched. Thus, update `mkukreloc.py`
    accordingly.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

---
+++ plat/common/include/x86/uk_reloc.h
@@ -0,0 +1,21 @@
+#ifndef __X86_UK_RELOC_H__
+#define __X86_UK_RELOC_H__
+
+#include <uk/plat/common/uk_reloc.h>
+
+#ifdef __ASSEMBLY__
+
+.macro ur_mov sym:req, reg:req, bytes:req, phys
+#ifdef CONFIG_OPTIMIZE_PIE
+	mov	$UK_RELOC_PLACEHOLDER, \reg
+.globl \sym\()_uk_reloc_imm\bytes\()\phys\()
+.set \sym\()_uk_reloc_imm\bytes\()\phys\(), . - \bytes
+	nop
+#else
+	mov	$\sym, \reg
+#endif
+.endm
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* __X86_UK_RELOC_H__ */
