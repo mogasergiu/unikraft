f7475a0ee66043d92001be47867028a5c09673f0
Sergiu Moga <sergiu.moga@protonmail.com>
2023-03-22 20:51:57 +0200

    plat/common: Implement a `struct uk_reloc` based self relocator
    Add `do_uk_reloc` a method that parses the `.uk_reloc` section and
    applies the relocations accordingly. As arguments, this method receives
    a base physical address `r_paddr` for relocations that employ the
    `UK_RELOC_FLAGS_PHYS_REL` flag and a base virtual address `r_vaddr`
    for those that do not want to be relocated against a physical address,
    but rather by a given virtual address (e.g. can be used with KASLR).
    
    A case that may explain the need for `r_paddr` and `r_vaddr` would be
    a situation where the previous program loader would place us at a random
    physical address (r_paddr) and we then want to apply our own virtual
    mappings for KASLR (r_vaddr), over the already applied virtual mappings,
    if any.
    
    Furthermore, before exiting the function, the initial, added throgh
    `mkbootinfo.py`, memory region descriptors are also relocated.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

---
+++ plat/common/uk_reloc.c
@@ -0,0 +1,47 @@
+#include <uk/plat/common/uk_reloc.h>
+
+#if defined(__X86_64__)
+#include <x86/cpu.h>
+#elif defined(__ARM_64__)
+#include <arm/cpu.h>
+#endif
+
+#include <uk/plat/memory.h>
+
+void __used do_uk_reloc(__paddr_t r_paddr, __vaddr_t r_vaddr)
+{
+	struct ukplat_memregion_desc *mrdp;
+	struct uk_reloc_hdr *ur_hdr;
+	struct uk_reloc *ur;
+	__u64 val;
+
+	/* Check .uk_reloc signature */
+	ur_hdr = (struct uk_reloc_hdr *)uk_reloc_sec;
+	while (ur_hdr->signature != UK_RELOC_SIGNATURE)
+		halt();
+
+	if (r_paddr == 0)
+		r_paddr = (__paddr_t)__BASE_ADDR;
+
+	if (r_vaddr == 0)
+		r_vaddr = (__vaddr_t)__BASE_ADDR;
+
+	for (ur = ur_hdr->urs; ur->r_sz; ur++) {
+		if (ur->flags & UK_RELOC_FLAGS_PHYS_REL)
+			val = (__u64)r_paddr + ur->r_val_off;
+		else
+			val = (__u64)r_vaddr + ur->r_val_off;
+
+		apply_uk_reloc(ur, val, (void *)__BASE_ADDR);
+	}
+
+	/* Since we may have been placed at a random physical address, adjust
+	 * the initial memory region descriptors added through mkbootinfo.py
+	 * since they contain the link-time addresses, relative to __BASE_ADDR
+	 */
+	ukplat_memregion_foreach(&mrdp, 0, 0, 0) {
+		mrdp->pbase -= (__paddr_t)__BASE_ADDR;
+		mrdp->pbase += r_paddr;
+		mrdp->vbase = mrdp->pbase;
+	}
+}
