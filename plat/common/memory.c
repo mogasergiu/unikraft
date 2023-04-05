152b74511b1516bcc99520a2da7af43cd62130ee
Sergiu Moga <sergiu.moga@protonmail.com>
2023-04-05 13:44:25 +0300

    plat/common: Generalize `bootmemory_palloc`
    Since `bootmemory_palloc` can be used by any platform and
    architecture and can come in handy when wanting to allocate a memory
    region descriptor on the sport, that is not part of the original
    memory map, move it to a more general location.
    
    Furthermore, rename the function to `ukplat_memregion_alloc` to make
    its usage-case more intuitive.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

--- plat/common/memory.c
+++ plat/common/memory.c
@@ -30,10 +30,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <uk/plat/memory.h>
-#include <uk/plat/common/memory.h>
+#include <uk/plat/common/bootinfo.h>
+#include <uk/asm/limits.h>
 #include <uk/alloc.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 static struct uk_alloc *plat_allocator;
 
@@ -55,3 +56,84 @@ struct uk_alloc *ukplat_memallocator_get(void)
 {
 	return plat_allocator;
 }
+
+
+#if !defined(LINUXUPLAT)
+#if defined(__X86_64__)
+#define PLATFORM_MAX_MEM_ADDR 0x00100000000 /* 4 GiB */
+#elif defined(__ARM_64__)
+#define PLATFORM_MAX_MEM_ADDR 0x10000000000 /* 512 GiB */
+#endif
+static inline bool is_in_static_pt(__paddr_t addr)
+{
+	return addr < PLATFORM_MAX_MEM_ADDR;
+}
+
+void *ukplat_memregion_alloc(__sz size, int type)
+{
+	struct ukplat_memregion_desc *mrd;
+	__paddr_t pstart, pend;
+	__paddr_t ostart, olen;
+	int rc;
+
+	size = ALIGN_UP(size, __PAGE_SIZE);
+	ukplat_memregion_foreach(&mrd, UKPLAT_MEMRT_FREE, 0, 0) {
+		UK_ASSERT(mrd->pbase <= __U64_MAX - size);
+		pstart = ALIGN_UP(mrd->pbase, __PAGE_SIZE);
+		pend   = pstart + size;
+
+		if (!is_in_static_pt(pend) || pend > mrd->pbase + mrd->len)
+			continue;
+
+		UK_ASSERT((mrd->flags & UKPLAT_MEMRF_PERMS) ==
+			  (UKPLAT_MEMRF_READ | UKPLAT_MEMRF_WRITE));
+
+		ostart = mrd->pbase;
+		olen   = mrd->len;
+
+		/* If fragmenting this memory region leaves it with length 0,
+		 * then simply overwrite and return it instead
+		 */
+		if (olen - (pstart - ostart) == size) {
+			mrd->pbase = pstart;
+			mrd->vbase = pstart;
+			mrd->len = pend - pstart;
+			mrd->type = type;
+			mrd->flags = UKPLAT_MEMRF_READ |
+				     UKPLAT_MEMRF_WRITE |
+				     UKPLAT_MEMRF_MAP;
+
+			return (void *)pstart;
+		}
+
+		/* Adjust free region */
+		mrd->len  -= pend - mrd->pbase;
+		mrd->pbase = pend;
+
+		mrd->vbase = (__vaddr_t)mrd->pbase;
+
+		/* Insert allocated region */
+		rc = ukplat_memregion_list_insert(&ukplat_bootinfo_get()->mrds,
+			&(struct ukplat_memregion_desc){
+				.vbase = pstart,
+				.pbase = pstart,
+				.len   = size,
+				.type  = type,
+				.flags = UKPLAT_MEMRF_READ |
+					 UKPLAT_MEMRF_WRITE |
+					 UKPLAT_MEMRF_MAP,
+			});
+		if (unlikely(rc < 0)) {
+			/* Restore original region */
+			mrd->vbase = ostart;
+			mrd->len   = olen;
+
+			return NULL;
+		}
+
+		return (void *)pstart;
+	}
+
+	return NULL;
+}
+#endif
