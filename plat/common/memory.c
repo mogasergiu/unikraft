c761e8024b982e1d50a6a36b6da5b634c2b2859c
Sergiu Moga <sergiu.moga@protonmail.com>
2023-04-08 12:57:03 +0300

    plat/common: make `ukplat_memregion_alloc` aware of memory holes
    In case we are somehow able to allocate memory from an in-image
    memory hole, make sure we do not give it the `UKPLAT_MEMRF_MAP`
    flag, since it is already mapped, which causes the paging
    initialization phase to exit with an `-EEXIST` error code.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

--- plat/common/memory.c
+++ plat/common/memory.c
@@ -30,6 +30,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <uk/plat/common/sections.h>
 #include <uk/plat/common/bootinfo.h>
 #include <uk/asm/limits.h>
 #include <uk/alloc.h>
@@ -91,6 +92,12 @@ void *ukplat_memregion_alloc(__sz size, int type, __u16 flags)
 		ostart = mrd->pbase;
 		olen   = mrd->len;
 
+		/* Check whether we are allocating from an in-image memory hole
+		 * or not. If no, then it is not already mapped.
+		 */
+		if (!RANGE_CONTAIN(__BASE_ADDR, __END - __BASE_ADDR, pstart, size))
+			flags |= UKPLAT_MEMRF_MAP;
+
 		/* If fragmenting this memory region leaves it with length 0,
 		 * then simply overwrite and return it instead.
 		 */
@@ -99,7 +106,7 @@ void *ukplat_memregion_alloc(__sz size, int type, __u16 flags)
 			mrd->vbase = pstart;
 			mrd->len = pend - pstart;
 			mrd->type = type;
-			mrd->flags = flags | UKPLAT_MEMRF_MAP;
+			mrd->flags = flags;
 
 			return (void *)pstart;
 		}
@@ -117,7 +124,7 @@ void *ukplat_memregion_alloc(__sz size, int type, __u16 flags)
 				.pbase = pstart,
 				.len   = size,
 				.type  = type,
-				.flags = flags | UKPLAT_MEMRF_MAP,
+				.flags = flags,
 			});
 		if (unlikely(rc < 0)) {
 			/* Restore original region */
