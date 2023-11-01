e77245e8126c753e146d3723dd70c8fe0d5b7d45
Sergiu Moga <sergiu@unikraft.io>
2023-11-01 09:40:59 +0200

    lib/syscall_shim: Introduce `struct ukarch_ulctx`
    Define an architecture specific userland context that an architecture
    can use to store things it may want to preserve or switch between
    when entering/exiting Unikraft context during normal userland
    execution (e.g. running in conjunction with binary syscalls).
    
    For now, this is defined as a struct with a base field for both
    ARM64 and x86_64, namely the `tpidr_el0` and `fs_base` registers
    respectively.
    
    Additionally, x86_64 has one more specfic field:
    - `gs_base`, that refers to the value of the `gs_base` register of
    the application before calling the system call
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

---
+++ lib/syscall_shim/arch/x86_64/include/uk/ulctx.h
@@ -0,0 +1,22 @@
+#ifndef __UK_SYSCALL_H__
+#error Do not include this header directly
+#endif
+
+#define UKARCH_ULCTX_SIZE			16
+
+#if !__ASSEMBLY__
+
+#include <uk/essentials.h>
+
+/* Architecture specific userland context */
+struct ukarch_ulctx {
+	/* The current value of %gs's gs_base register of the application.
+	 * On syscall entry, this will be updated to hold the value of
+	 * MSR_KERNEL_GS_BASE following a swapgs instruction */
+	__uptr gs_base;
+
+	__uptr fs_base;
+};
+UK_CTASSERT(sizeof(struct ukarch_ulctx) == UKARCH_ULCTX_SIZE);
+
+#endif /* !__ASSEMBLY__ */
