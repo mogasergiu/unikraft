7452ce0d1d2aadd7003d116ed4e2197df7602e3d
Sergiu Moga <sergiu@unikraft.io>
2023-11-01 11:10:10 +0200

    lib/syscall_shim/arch/x86_64: Add `ulctx` `switchon`/`switchoff`
    Implement `switchoff`/`switchon` functions to be used in conjunction
    with the userland context.
    
    The `switchoff` operation assumes that it can only be called from within
    Unikraft context (i.e. not directly by the app) and, as such, the
    app's `gs_base` register value was preserved within
    `X86_MSR_KERNEL_GS_BASE` following a `swapgs` on syscall entry.
    This value will be thus stored in the current userland
    context.
    
    The `switchon` operation assumes the same thing and, therefore, it will
    set `X86_MSR_GS_BASE` to the current `struct lcpu` and
    `X86_MSR_KERNEL_GS_BASE` to the app's preserved `gs_base` register
    within the userland context of the app's thread we are currently
    switching to.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

--- lib/syscall_shim/arch/x86_64/ulctx.c
+++ lib/syscall_shim/arch/x86_64/ulctx.c
@@ -10,7 +10,43 @@
 #include <uk/essentials.h>
 #include <uk/plat/common/cpu.h>
 #include <uk/thread.h>
+#include <x86/gsbase.h>
 #include <uk/syscall.h>
+
+void ukarch_ulctx_switchoff(struct ukarch_ulctx *ulctx)
+{
+	UK_ASSERT(ulctx);
+	UK_ASSERT(lcpu_get_current());
+	UK_ASSERT(!IS_LCPU_PTR(rdmsrl(X86_MSR_KERNEL_GS_BASE)));
+
+	/* This can only be called from Unikraft ctx in bincompat mode.
+	 * Therefore, X86_MSR_GS_BASE holds the current `struct lcpu` and
+	 * X86_MSR_KERNEL_GS_BASE contains the app-saved gs_base.
+	 */
+	ulctx->gs_base = rdkgsbase();
+
+#if CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS
+	ukarch_ulctx_switchoff_tls(ulctx);
+#endif /* CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS */
+}
+
+void ukarch_ulctx_switchon(struct ukarch_ulctx *ulctx)
+{
+	UK_ASSERT(ulctx);
+	UK_ASSERT(lcpu_get_current());
+	UK_ASSERT(IS_LCPU_PTR(rdmsrl(X86_MSR_KERNEL_GS_BASE)));
+
+#if CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS
+	ukarch_ulctx_switchon_tls(ulctx);
+#endif /* CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS */
+
+	/* This can only be called from Unikraft ctx in bincompat mode.
+	 * Therefore, X86_MSR_GS_BASE holds the current `struct lcpu` and
+	 * X86_MSR_KERNEL_GS_BASE contains the app-saved gs_base.
+	 */
+	wrgsbase((__uptr)lcpu_get_current());
+	wrkgsbase(ulctx->gs_base);
+}
 
 #if CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS
 __uptr ukarch_ulctx_get_tlsp(struct ukarch_ulctx *ulctx)
