ab6a217aeaec3e1211ed4f77523e01604df336fe
Sergiu Moga <sergiu@unikraft.io>
2023-11-05 12:13:12 +0200

    lib/syscall_shim/arch/arm64: Add `tpidr_el0` getter/setter for `ulctx`
    Add basic setter/getter operations for the `tpidr_el0` field of the
    userland context structure that is meant to represent the userspace
    TLS.
    
    Signed-off-by: Sergiu Moga <sergiu@unikraft.io>

---
+++ lib/syscall_shim/arch/arm64/ulctx.c
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+#include <uk/arch/ctx.h>
+#include <uk/arch/lcpu.h>
+#include <uk/arch/types.h>
+#include <uk/assert.h>
+#include <uk/essentials.h>
+#include <uk/plat/common/cpu.h>
+#include <uk/thread.h>
+#include <uk/syscall.h>
+
+#if CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS
+__uptr ukarch_ulctx_get_tlsp(struct ukarch_ulctx *ulctx)
+{
+	UK_ASSERT(ulctx);
+
+	return ulctx->tpidr_el0;
+}
+
+void ukarch_ulctx_set_tlsp(struct ukarch_ulctx *ulctx, __uptr tlsp)
+{
+	UK_ASSERT(ulctx);
+
+	uk_pr_debug("System call updated userland TLS pointer register to %p (before: %p)\n",
+		    (void *)ulctx->tpidr_el0, (void *)tlsp);
+
+	ulctx->tpidr_el0 = tlsp;
+}
+#endif /* CONFIG_LIBSYSCALL_SHIM_HANDLER_ULTLS */
